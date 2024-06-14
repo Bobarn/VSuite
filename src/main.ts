bbc0fe73f69b9c31e9d49d8366f4310d1b8f1314
Brandon <b.n.tamayo7@gmail.com>
2024-06-13 18:00:34 -0700

    Added in the first loaded maps from Tiled

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './HelloWorldScene'
+import GameScene from './scenes/GameScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	parent: 'app',
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 },
-		},
-	},
-	scene: [HelloWorldScene],
+  backgroundColor: '#CFF5FC',
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelArt: true, // Prevent pixel art from becoming blurred when scaled.
+  scale: {
+    mode: Phaser.Scale.ScaleModes.RESIZE,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    min: {
+      width: 800,
+      height: 600,
+    },
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  autoFocus: true,
+  scene: [GameScene],
 }
 
 export default new Phaser.Game(config)
